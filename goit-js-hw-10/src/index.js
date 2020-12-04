7607422656f4aba1c766edc4e7473300f8e9948f
Aleksander-Kovalenko <coldfellow@gmail.com>
2020-12-04 17:17:44 +0200

    Шаблонна карточек

--- goit-js-hw-10/src/index.js
+++ goit-js-hw-10/src/index.js
@@ -1,1 +1,11 @@
 import './styles.css';
+import menuFood from './menu.json';
+import templateCards from './template/templateMenuCard.hbs';
+
+const creatFoodCards = templateCards(menuFood);
+
+const refs = {
+  menuFood: document.querySelector('.js-menu'),
+};
+
+refs.menuFood.insertAdjacentHTML('beforeend', creatFoodCards);
