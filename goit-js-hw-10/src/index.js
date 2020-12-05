903a054a5e48dad17aa19e828fb21c4e15fb6fb9
Aleksander-Kovalenko <coldfellow@gmail.com>
2020-12-05 14:29:54 +0200

    Добавил сохранения положения checked

--- goit-js-hw-10/src/index.js
+++ goit-js-hw-10/src/index.js
@@ -2,19 +2,21 @@ import './styles.css';
 import menuFood from './menu.json';
 import templateCards from './template/templateMenuCard.hbs';
 
+const LOCAL_THEME_KEY = 'choice-theme';
 const creatFoodCards = templateCards(menuFood);
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
+localStorageTheme();
+
 const refs = {
   menuFood: document.querySelector('.js-menu'),
-  switch: document.querySelector('.theme-switch input'),
+  switch: document.querySelector('#theme-switch-toggle'),
 };
 
 refs.menuFood.insertAdjacentHTML('beforeend', creatFoodCards);
-
 refs.switch.addEventListener('change', choiceTheme);
 
 function choiceTheme(e) {
@@ -24,12 +26,23 @@ function choiceTheme(e) {
     lightTheme();
   }
 }
+
 function darkTheme() {
   document.body.classList.remove(Theme.LIGHT);
   document.body.classList.add(Theme.DARK);
+  localStorage.setItem(LOCAL_THEME_KEY, Theme.DARK);
 }
 
 function lightTheme() {
   document.body.classList.remove(Theme.DARK);
   document.body.classList.add(Theme.LIGHT);
+  localStorage.setItem(LOCAL_THEME_KEY, Theme.LIGHT);
+}
+
+function localStorageTheme() {
+  const getLocal = localStorage.getItem(LOCAL_THEME_KEY);
+  document.body.classList.add(getLocal);
+  if (getLocal === Theme.DARK) {
+    document.querySelector('#theme-switch-toggle').checked = true;
+  }
 }
